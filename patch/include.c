e28d8f4d996fbb9e568f7d8e740886eb0b564475
a-mere-peasant <harshjain075@gmail.com>
2021-11-03 16:39:36 +0530

    START

---
+++ patch/include.c
@@ -0,0 +1,27 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_alpha.c"
+#include "bar_dwmblocks.c"
+#include "bar_ltsymbol.c"
+#include "bar_powerline_status.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_statuscmd.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "fullscreen.c"
+#include "movestack.c"
+#include "pertag.c"
+#include "scratchpad.c"
+#include "sticky.c"
+#include "swallow.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,26 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_alpha.h"
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_powerline_status.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "fullscreen.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "scratchpad.h"
+#include "sticky.h"
+#include "swallow.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
